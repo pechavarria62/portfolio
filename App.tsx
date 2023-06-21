786639d0cd2889a868019400668d6b59cb2edf87
Pedro Echavarria <pechavarria62@gmail.com>
2023-06-21 04:23:51 -0400

    adding bottom navigation tabs then a drawer navigation

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 // import Index from './src/pages/Index'
 import {NavigationContainer} from '@react-navigation/native';
-import Navigation from "../frontend/src/components/Navigation";
+import Navigation from './src/components/Navigation';
 
 
 export default function App() {
